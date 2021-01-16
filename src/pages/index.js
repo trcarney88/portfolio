8f01d85fb13583dff3cac7cb072030e0ac29b947
Todd Carney <toddcarney44@gmail.com>
2021-01-16 15:13:15 -0700

    Initial changes to gatsby starter

---
+++ src/pages/index.js
@@ -0,0 +1,92 @@
+import React, { useContext } from "react"
+import { graphql } from "gatsby"
+import ThemeContext from "../utils/theme"
+import { PageLayout } from "../components"
+import { SEO } from "../utils"
+import { Container } from "react-bootstrap"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+
+export default ({ data }) => {
+  const { firstName, lastName, occupation } = data.site.siteMetadata
+  const { dark } = useContext(ThemeContext)
+  return (
+    <PageLayout>
+      <SEO title="Home" />
+      <Container className="text-center pt-5 mt-5" fluid>
+        <Container className="py-0 my-0">
+          <h1
+            style={{
+              fontSize: "5rem",
+              color: "black",
+            }}
+          >
+            <span className="first-name">{firstName}</span>&nbsp;
+            <span className="last-name">{lastName}</span>
+          </h1>
+          <p>
+            <i>
+              {occupation}
+            </i>
+          </p>
+        </Container>
+        <hr className="my-3 w-25" />
+        <div className="d-md-inline-flex icons-container">
+          <a
+            href="https://github.com/trcarney88"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={["fab", "github"]}
+              className="icons github"
+              title="Github"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/todd-carney-56494062/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={["fab", "linkedin"]}
+              className="icons linkedin"
+              title="LinkedIn"
+            />
+          </a>
+          <a
+            href="mailto:toddcarney44@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={["fas", "envelope"]}
+              className="icons mail"
+              title="e-mail"
+            />
+          </a>
+          <a href="../../resume.pdf" target="_blank" download>
+            <FontAwesomeIcon
+              icon={["fas", "file-alt"]}
+              className="icons file"
+              title="Resume"
+            />
+          </a>
+        </div>
+      </Container>
+    </PageLayout>
+  )
+}
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        unemployed
+        firstName
+        lastName
+        occupation
+      }
+    }
+  }
+`
