c334aaffe758743366e48194c207842441ab2b03
Todd Carney <toddcarney44@gmail.com>
2025-02-12 20:47:39 -0700

    change sst project name

--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: "portfolio-astro",
+      name: "portfolio",
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
